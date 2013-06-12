bcb635fd42cf8b6bcd7bbcd3dc87823fe950761c
Tarun Bhardwaj <tarun.bhardwaj@openlabs.co.in>
2013-06-12 08:21:55 +0530

    Updated scroll

--- js/app.js
+++ js/app.js
@@ -10,4 +10,22 @@ $(document).ready(function(){
         $wrap.toggleClass('active');
         return false;
         });
+
+      $(".my-affix").css("position", "relative");
+      $(window).on('resize scroll', function() {
+        var scroll_top = $(window).scrollTop();
+        var sticky_offset_top = 0;
+        var sticky_offset_bottom = 314;
+        sticky_offset_bottom = $(document).height() - sticky_offset_bottom - $(".my-affix").height();
+        console.log(sticky_offset_bottom);
+        if (scroll_top > sticky_offset_top && scroll_top < sticky_offset_bottom ) {
+            $(".my-affix").animate({top:scroll_top + "px"},{duration:500,queue:false});
+        }
+        else if (scroll_top > sticky_offset_top && scroll_top > sticky_offset_bottom ) {
+            $(".my-affix").animate({top:sticky_offset_bottom + "px"},{duration:500,queue:false});
+        }
+        else{
+            $(".my-affix").animate({top:sticky_offset_top + "px"},{duration:500,queue:false});
+        }
+      }).resize();
 });
